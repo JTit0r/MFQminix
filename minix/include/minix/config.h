162f39366bd8dff486f6fe5c4f4428301f672f43
JTit0r <32867770+JTit0r@users.noreply.github.com>
2023-05-11 13:56:53 -0300

    MAX_USER_Q 19 -> 0

--- minix/include/minix/config.h
+++ minix/include/minix/config.h
@@ -66,7 +66,7 @@
 //adicionadas mais tres filas para o MFQ (16 -> 19). Prioridade maxima de usuario alterada (0 -> 19)
 #define NR_SCHED_QUEUES   19	/* MUST equal minimum priority + 1 */
 #define TASK_Q		   0	/* highest, used for kernel tasks */
-#define MAX_USER_Q  	   19    /* highest priority for user processes */   
+#define MAX_USER_Q  	   0    /* highest priority for user processes */   
 #define USER_Q  	  ((MIN_USER_Q - MAX_USER_Q) / 2 + MAX_USER_Q) /* default
 						(should correspond to nice 0) */
 #define MIN_USER_Q	  (NR_SCHED_QUEUES - 1)	/* minimum priority for user
